f387a698c6c220da71b0dcf9ec372afd0c7562dd
mekindog <mekindog@gmail.com>
2025-11-01 13:26:25 +0000

    MainUpdate

---
+++ Scripts/camera/subpixelcamera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var actual_cam_pos : Vector2
+@onready var player : CharacterBody2D = $"../PlayerContainer/player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	actual_cam_pos = actual_cam_pos.lerp(player.position,delta*3)
+	var cam_subpixel_offset = actual_cam_pos.round() - actual_cam_pos
+	get_parent().get_parent().get_parent().get_parent().material.set_shader_parameter("cam_offset",cam_subpixel_offset)
+	global_position = actual_cam_pos.round()
