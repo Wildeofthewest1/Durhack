4fe505803d427b8f8ad7f5c17599a81855720d2e
THEREALTOLMO <tolmgaming@gmail.com>
2025-11-01 21:15:34 +0000

    update

--- Scenes/planet.gd
+++ Scenes/planet.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var mass = 100.00
+@export var mass = 10.00
+
 @export var initial_speed = 0
 @export var initial_direction = Vector2.ZERO 
 
@@ -9,19 +10,19 @@ extends CharacterBody2D
 @export var parent_star: CharacterBody2D #Add the host star of the system
 @export var parent_planet: CharacterBody2D #Add planetary moons 
 
-var gravity_scale = 5e2
+var gravity_scale = 7e2
 
 func _ready():
 	if not autoorbit:
 		velocity = initial_speed * initial_direction
 	if autoorbit and not is_in_group("Star"):
 		#times 60 because of the physics framerate
-		var speed = (60 * parent_star.mass * gravity_scale/(position - parent_star.position).length())**(0.5)
-		var direction = (position - parent_star.position).normalized()
+		var speed = (60 * parent_star.mass * gravity_scale/(global_position - parent_star.global_position).length())**(0.5)
+		var direction = (global_position - parent_star.global_position).normalized()
 		velocity = speed * direction.orthogonal()
 		if is_in_group("Moon"):
 			var planet_speed = parent_planet.velocity
-			speed = (60 * parent_planet.mass * gravity_scale/(position - parent_planet.position).length())**(0.5)
+			speed = (60 * parent_planet.mass * gravity_scale/(global_position - parent_planet.global_position).length())**(0.5)
 			velocity = speed * direction.orthogonal() + planet_speed
 
 
@@ -29,7 +30,7 @@ func _process(_delta: float) -> void:
 	#print(name, " ", (position - orbital_parent.position).length())
 	if not is_in_group("Star"):
 		#print((position - parent_star.position).length())
-		print(velocity.length())
+		#print(velocity.length())
 		pass
 	pass
 func _physics_process(delta: float) -> void:
@@ -40,7 +41,7 @@ func _physics_process(delta: float) -> void:
 func force_g():
 	for i in range(len(planets)):
 		if i != get_index():
-			var direction_g = position - planets[i].global_position
+			var direction_g = global_position - planets[i].global_position
 			if direction_g.length() > 3:
 				velocity -= (direction_g.normalized()
 								* planets[i].mass * gravity_scale/direction_g.length()**2)
--- Scenes/space_station.gd
+++ Scenes/space_station.gd
@@ -1,41 +1,37 @@
 extends CharacterBody2D
 
 #Custom Intial Conditions
-@export var mass = 100.00
 @export var initial_speed = 0
 @export var initial_direction = Vector2.ZERO 
 
-
-@export var parent_planet: CharacterBody2D #Add planetary moons 
 @export var autoorbit = true
+@export var parent_planet: CharacterBody2D #Add planetary moons 
 var gravity_scale = 5e2
 
 func _ready():
 	if not autoorbit:
 		velocity = initial_speed * initial_direction
-	if autoorbit:
-		var direction = (position - parent_planet.position)
-		var planet_speed = parent_planet.velocity
-		var speed = (60 * parent_planet.mass * gravity_scale/direction.length())**(0.5)
-		velocity = speed * direction.normalized().orthogonal() + planet_speed
+	#if autoorbit:
+		#var direction = (position - parent_planet.global_position)
+		#var planet_speed = parent_planet.velocity
+		#var speed = (60 * parent_planet.mass * gravity_scale/direction.length())**(0.5)
+		#velocity = speed * direction.normalized().orthogonal() + planet_speed
 
 
 func _process(_delta: float) -> void:
 	#print(name, " ", (position - orbital_parent.position).length())
 	if not is_in_group("Star"):
 		#print((position - parent_star.position).length())
-		print(velocity.length())
+		#print(velocity.length())
 		pass
 	pass
+	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
-	force_g()
+	#force_g()
 
-@onready var planets =  get_tree().get_nodes_in_group("Planets")
 func force_g():
-	for i in range(len(planets)):
-		if i != get_index():
-			var direction_g = position - planets[i].global_position
-			if direction_g.length() > 3:
-				velocity -= (direction_g.normalized()
-								* planets[i].mass * gravity_scale/direction_g.length()**2)
+	var direction_g = global_position - parent_planet.global_position
+	if direction_g.length() > 3:
+		velocity -= (direction_g.normalized()
+						* parent_planet.mass * gravity_scale/direction_g.length()**2)
