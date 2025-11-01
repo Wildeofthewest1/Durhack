4ae1fffcaebbb079a6569d3ad884b1422e8badd1
THEREALTOLMO <tolmgaming@gmail.com>
2025-11-01 18:49:59 +0000

    Added AutoOrbit option for planets

--- Scenes/planet.gd
+++ Scenes/planet.gd
@@ -2,12 +2,28 @@ extends CharacterBody2D
 
 @export var mass = 100.00
 @export var initial_speed = 0
-@export var initial_direction = Vector2.ZERO
+@export var initial_direction = Vector2.ZERO 
 
-
+@export var autoorbit = false
+@export var orbital_parent: CharacterBody2D
 func _ready():
-	velocity = initial_speed * initial_direction
+	if not autoorbit:
+		velocity = initial_speed * initial_direction
+	if autoorbit and not is_in_group("Star"):
+		#times 60 because of the physics framerate
+		var speed = (60 * orbital_parent.mass * 1e4/(position - orbital_parent.position).length())**(0.5)
+		print(speed)
+		var direction = (position - orbital_parent.position).normalized()
+		velocity = speed * direction.orthogonal()
 
+
+func _process(_delta: float) -> void:
+	#print(name, " ", (position - orbital_parent.position).length())
+	#if not is_in_group("Star"):
+		##print((orbital_parent.mass 
+				##* 1e4 * 30/(position - orbital_parent.position).length())**(0.5))
+		#pass
+	pass
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 	force_g()
