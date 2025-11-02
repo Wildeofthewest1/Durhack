4c3782c52ec4cec4af0a8134c6b9748f281d19b1
mekindog <mekindog@gmail.com>
2025-11-02 11:36:37 +0000

    ekin pre final

--- 400_years.gd
+++ 400_years.gd
@@ -1,16 +1,16 @@
 extends Label
 
 
-var timevalue = 400
-@export var timescale = 2 #in seconds
+var timevalue = 400.0
+@export var timescale = 0.2 #in seconds
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Timer.wait_time = timescale
-	text = "Years Until Invasion: " + str(timevalue)
+	text = str(snapped(timevalue,0.1)) + " Years Until the Invasion" 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	text = "Years Until Invasion: " + str(timevalue)
+	text = str(snapped(timevalue,0.1)) + " Years Until the Invasion" 
 	
 
 func _on_timer_timeout() -> void:
-	timevalue -= 1
+	timevalue -= 0.1
